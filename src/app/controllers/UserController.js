3259db838057f30d6547b9184230a8ded47e2b9d
marciovz <marciovz@yahoo.com.br>
2019-10-18 10:02:45 -0300

    Update do usuário - implementado a funcionalidade de update do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,9 +21,30 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    // Verifica se o novo email já existe
+    if (email !== user.email) {
+      const userExistes = await User.findOne({ where: { email } });
+      if (userExistes) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    // Verifica se veio o oldPassword na requisição, e se ela é valida permite update
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      provider,
+    });
   }
 }
 
