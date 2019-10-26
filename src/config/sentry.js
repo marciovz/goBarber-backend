379590a51629c18f2b095f5738b18d69aba1dd9a
marciovz <marciovz@yahoo.com.br>
2019-10-25 22:58:50 -0300

    Criando variáveis de ambiente

--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,4 @@
 export default {
-  dsn: 'https://8794e3def7fc4c39b94b96dde1193561@sentry.io/1796909',
+  // dsn: 'https://8794e3def7fc4c39b94b96dde1193561@sentry.io/1796909',
+  dsn: process.env.SENTRY_DSN,
 };
